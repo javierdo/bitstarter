cb3c1e406132470a3b4bc5d1df675a49ffef7e02
yintodo <javier.dolado@ehu.es>
2013-07-06 22:16:06 +0200

    web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var buffer = new Buffer(40);
+	var buffer = new Buffer(80);
 	buffer = fs.readFileSync('index.html');
   response.send(buffer.toString('utf8',0,16));
   
