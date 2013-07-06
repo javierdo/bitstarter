785c78a988360f73aaef40beb2449029ab3766fb
yintodo <javier.dolado@ehu.es>
2013-07-06 22:05:18 +0200

    new web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var buffer = new Buffer(16);
 	buffer = fs.readFileSync('index.html');
-  response.send(buf.toString('utf8',0,16));
+  response.send(buffer.toString('utf8',0,16));
   
 });
 
